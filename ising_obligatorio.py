482b33a043596d3a4831220cf76a49cd747d68c0
anardgz01 <anarodriguezartacho@gmail.com>
2024-04-12 12:07:39 +0200

    minor style fix

--- ising_obligatorio.py
+++ ising_obligatorio.py
@@ -50,7 +50,7 @@ def simulate(sorted : bool, T : float, N : int, num_Monte_Carlo_steps : int, pat
         return np.sum(conf)/N**2
 
     #Start iterating
-    for t in range(1,iterations):
+    for t in range(1,iterations+1):
         pos = (np.random.randint(0, N), np.random.randint(0, N))
         probability = p(pos)
         ksi = np.random.uniform(0.,1.)
@@ -66,8 +66,6 @@ def simulate(sorted : bool, T : float, N : int, num_Monte_Carlo_steps : int, pat
 
         if t % 10000 == 0:
             print(f'Iteration number {t+1} of {iterations-1}')
-    confs_mcs[-1] = conf
-    magnetizations_mcs[-1] = magnetization(conf)
 
     np.save(f'resultados/confs_{path}.npy', confs_mcs)
     np.save(f'resultados/mags_{path}.npy', magnetizations_mcs)
