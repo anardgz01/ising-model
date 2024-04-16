58f25fcde52cb38656852b508e9b51e669720d4f
anardgz01 <anarodriguezartacho@gmail.com>
2024-04-16 16:39:45 +0200

    Update ising_obligatorio_mag.py

--- ising_obligatorio_mag.py
+++ ising_obligatorio_mag.py
@@ -2,8 +2,9 @@ import numpy as np
 import ising_simulator as ising
 import os
 import json
+import plot_animation_ising as myplot
 
-N = 64
+N = 16
 T = np.random.uniform(1.5, 3.5, 10)
 T = np.sort(T)
 
@@ -17,8 +18,7 @@ for filename in os.listdir(folder_path):
 def average_magnetization(file : str) -> tuple[float, float]:
         mags = np.load(file)
         # mags_100 = mags[np.arange(99, len(mags), 100)]
-        mags_100 = mags[np.arange(3, len(mags), 4)]
-        print(mags_100)
+        mags_100 = mags[np.arange(99, len(mags), 100)]
         
         average = np.mean(mags_100)
         error = np.std(mags_100)
@@ -28,10 +28,11 @@ mags_dict = dict()
 
 for t in T:
     path = f'temp_{t:.2f}'
-    ising.simulate(True, t, N, 10, path)
-    mags_dict[(N,t)] = average_magnetization(f'resultados/mags_{path}.npy')
+    ising.simulate(True, t, N, 10000, path)
+    mags_dict[t] = average_magnetization(f'resultados/mags_{path}.npy')
 
-mags_dict_str_keys = {str(key).replace(' ', ''): value for key, value in mags_dict.items()}
+# mags_dict_str_keys = {str(key).replace(' ', ''): value for key, value in mags_dict.items()}
 with open('resultados/mags_dict.json', 'w') as f:
-    json.dump(mags_dict_str_keys, f, indent=4)
+    json.dump(mags_dict, f, indent=4)
 
+myplot.plot_mag()
