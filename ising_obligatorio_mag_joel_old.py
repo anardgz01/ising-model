6d5b4fe95593c5e7ec042731d727ce301861bdad
anardgz01 <anarodriguezartacho@gmail.com>
2024-04-23 11:47:54 +0200

    Create backup files and cleaned up code
    Improved and faster version of simulator. T is now a linspace so deleting resultados is not needed, expected value is calculated in an average-like way, time is printed, and several new fixes like first magnetization not being changed.

--- ising_obligatorio_mag_joel_old.py
+++ ising_obligatorio_mag_joel_old.py
@@ -1,16 +1,25 @@
 import numpy as np
 import ising_simulator_beta as ising
+# import os
 
 N = 32
 T = np.linspace(0.01, 5, 10)
 T = np.sort(T)
 mags_exps = np.zeros((3, len(T)))
 
+# Delete all contents in 'resultados' folder
+# folder_path = '/home/anardgz01/Documentos/GitHub/ising-model/resultados'
+# for filename in os.listdir(folder_path):
+#     file_path = os.path.join(folder_path, filename)
+#     if os.path.isfile(file_path):
+#         os.unlink(file_path)
+
 def expected_value(file_mags : str):
         mags = np.load(file_mags)
         print(f'len mags es {len(mags)}')
         
         hist, bin_edges = np.histogram(mags, bins=10000, range=(0,1.0001))
+        # bin_edges[-1] = 1.01
         hist = hist/len(mags)
         print(f'len hist es {len(hist)}')
         print(f'mags is {mags}')
@@ -32,6 +41,12 @@ def expected_value(file_mags : str):
 def average_magnetization(file_mags : str):
     mags = np.load(file_mags)
     return np.mean(mags)
+
+# def average_magnetizations(file_mags : str, file_probs : str) -> tuple[float, float]:
+#         mags = np.load(file_mags)
+#         probs = np.load(file_probs)
+#         mags_avgs = np.sum(probs*mags)
+#         return mags_avgs
 
 for index, t in enumerate(T):
     path = f'temp_{t:.2f}'
--- ising_simulator_beta_old_b_factors.py
+++ ising_simulator_beta_old_b_factors.py
@@ -7,7 +7,9 @@ import time
 def simulate(sorted : bool, T : float, N : int, num_Monte_Carlo_steps : int, path : str):
     Monte_Carlo_step = N**2
     iterations = num_Monte_Carlo_steps*Monte_Carlo_step
+    # confs = np.zeros((iterations,N,N), dtype=int)
     magnetizations_mcs = np.zeros((num_Monte_Carlo_steps//100))
+    # factor_conf_mcs = np.zeros((num_Monte_Carlo_steps//100+1))
 
     #Create the initial state
     if sorted:
@@ -16,6 +18,9 @@ def simulate(sorted : bool, T : float, N : int, num_Monte_Carlo_steps : int, pat
     else:
         conf = np.random.choice([1, -1], size=(N, N))
         print('unsorted')
+    # confs[0] = conf
+
+    #print(confs[0])
 
     #Functions
     def get_neighbourhood(pos : tuple[int,int]):
@@ -41,12 +46,26 @@ def simulate(sorted : bool, T : float, N : int, num_Monte_Carlo_steps : int, pat
     def magnetization(conf):
         '''Return the magnetization of the system'''
         return np.abs(np.sum(conf)/N**2)
+    
+    # def energy_conf(conf):
+    #     energy = 0.
+    #     for i in range(N):
+    #         for j in range(N):
+    #             pos_nn = get_neighbourhood((i,j))
+    #             energy += -0.5*conf[pos_nn[0]] * (conf[pos_nn[1]] + conf[pos_nn[2]] + conf[pos_nn[3]] + conf[pos_nn[4]])
+    #     return energy
+    
+    # def factor_conf(conf):
+    #     energy = energy_conf(conf)
+    #     print(energy)
+    #     factor = np.e**(-energy/T)
+    #     print(factor)
+    #     return factor
 
     percent = 2
-    landmark = iterations // (100//percent) #print progress every {percent}%
-    start_time = time.time()
-    
+    landmark = iterations // (100//percent) #print progress every 2%
     #Start iterating
+    start_time = time.time()
     for t in range(1,iterations+1):
         pos = (np.random.randint(0, N), np.random.randint(0, N))
         probability = p(pos)
@@ -54,12 +73,21 @@ def simulate(sorted : bool, T : float, N : int, num_Monte_Carlo_steps : int, pat
 
         if ksi < probability:
             conf[pos] *= -1
+
+        # confs[t] = conf
         
         if t % (Monte_Carlo_step*100) == 0:
             magnetizations_mcs[t//(Monte_Carlo_step*100)-1] = magnetization(conf)
+            # factor_conf_mcs[t//(Monte_Carlo_step*100)] = factor_conf(conf)
 
         if t % landmark == 0:
             print(f'Iteration number {t+1} of {iterations} ({(t // landmark)*percent}% completed)')
     print(f'simulation finished in {time.time() - start_time}')
 
+    # probability_mcs = factor_conf_mcs/np.sum(factor_conf_mcs)
+
     np.save(f'resultados/mags_{path}.npy', magnetizations_mcs)
+
+
+    # np.save(f'resultados/probs_{path}.npy', probability_mcs)
+
